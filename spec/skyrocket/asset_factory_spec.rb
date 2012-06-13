da862f1e8838d9ee5b4e31ccf421bdc964790bb6
Michael M. Wasser <michael@raveld.com>
2012-06-13 15:55:09 -0700

    correcting asset_factory to allow 'from_name' using exact name including processor extensions

--- spec/skyrocket/asset_factory_spec.rb
+++ spec/skyrocket/asset_factory_spec.rb
@@ -15,13 +15,28 @@ describe Skyrocket::AssetFactory do
                       .with('/one', 'joe/shmo.html.erb', '/three', an_instance_of(Skyrocket::ErbProcessor))
                       .and_return('You Got Me2')
     Dir.stub('[]').with('/one/joe/shmo.html.*').and_return(['/one/joe/shmo.html.erb'])
+    Dir.stub('[]').with('/one/joe/shmo.html').and_return([])
     Dir.stub('[]').with('/two/joe/shmo.html.*').and_return([])
+    Dir.stub('[]').with('/two/joe/shmo.html').and_return([])
     factory.from_name('joe/shmo.html').should == "You Got Me2"
+  end
+
+  it 'should return an asset based off the asset name if the name matches completely' do
+    Skyrocket::Asset.stub(:new)
+                      .with('/one', 'joe/shmo.html.erb', '/three', an_instance_of(Skyrocket::ErbProcessor))
+                      .and_return('You Got Me2')
+    Dir.stub('[]').with('/one/joe/shmo.html.erb.*').and_return([])
+    Dir.stub('[]').with('/one/joe/shmo.html.erb').and_return(['/one/joe/shmo.html.erb'])
+    Dir.stub('[]').with('/two/joe/shmo.html.erb.*').and_return([])
+    Dir.stub('[]').with('/two/joe/shmo.html.erb').and_return([])
+    factory.from_name('joe/shmo.html.erb').should == "You Got Me2"
   end
 
   it 'should fail if asset is not found based off of asset name' do
     Dir.stub('[]').with('/one/joe/shmo.html.*').and_return([])
+    Dir.stub('[]').with('/one/joe/shmo.html').and_return([])
     Dir.stub('[]').with('/two/joe/shmo.html.*').and_return([])
+    Dir.stub('[]').with('/two/joe/shmo.html').and_return([])
     lambda { factory.from_name('joe/shmo.html') }.should raise_error(Skyrocket::AssetNotFoundError) 
   end
 
